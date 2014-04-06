e31a92ffbfc297b4e46c34d4b70b849b0716d1ce
wolf <wbiker@gmx.at>
2014-04-06 20:11:37 +0200

    New DB table

--- Document.pm
+++ Document.pm
@@ -1,12 +1,45 @@
 package Document;
 use Moose;
+use Digest::MD5 qw(md5_hex);
+use File::Basename;
+use File::Spec::Functions qw(catfile catdir);
+use File::Copy;
 
-has 'hash' => (is => 'ro', isa => 'Str');
-has 'filename' => (is => 'ro', isa => 'Str', required => 1);
-has 'filepath' => (is => 'ro', isa => 'Str', required => 1);
-has 'pathname' => (is => 'ro', isa => 'Str', required => 1);
-has 'category' => (is => 'ro', isa => 'ArrayRef');
+has 'hash' => (is => 'rw', isa => 'Str');
+has 'filename' => (is => 'rw', isa => 'Str');
+has 'file' => (is => 'ro', isa => 'Str', required => 1);
+has 'extension' => (is => 'rw', isa => 'Str');
+has 'path' => (is => 'rw', isa => 'Str');
 has 'tag' => (is => 'ro', isa => 'ArrayRef');
 has 'version' => (is => 'ro', isa => 'Int', default => 1);
+
+sub BUILD {
+	my $self = shift;
+	
+	# search for the file name and the base name
+	my ($filename, $directories, $extension) = fileparse($self->file, qr/\.[^.]*/);
+	$self->filename($filename);
+	$self->path($directories);
+	$self->extension($extension);
+	# calculate hash out of the name
+	$self->hash(md5_hex($filename));
+}
+
+sub store_file {
+	my $self = shift;
+	my $path = shift;
+	
+	my $dest_path = catdir($path, $self->hash);
+	mkdir($dest_path);
+	my $dest_file = catfile($dest_path, $self->{filename});
+	copy($self->{file}, $dest_file);
+}
+
+sub add_tag {
+	my $self = shift;
+	my $tag = shift;
+	
+	push(@{$self->{tag}}, $tag);
+}
 
 1;
--- pdms.pl
+++ pdms.pl
@@ -2,25 +2,70 @@ use strict;
 use warnings;
 use Getopt::Long;
 use feature qw(say);
+use YAML::Any qw(LoadFile);
 
 use Document;
-use Category;
 use Tag;
 use SqlManager;
 
+# read config.
+my $config = 'config.yaml';
+die "$config not found." unless -e $config;
+
+my $conf = LoadFile($config);
+die "No config read." unless $conf;
+
 # Command Line parameter handling
+my $search;
+my $check_in;
+my $check_out;
+my $list;
+my @file;
+my $tags;
+my $special; # in this variable are parameter stored that was on the command line without options.
 
-print "Initialize SQL... ";
-my $sql = new SqlManager->new;
-say "OK";
+GetOptions(
+	"search" => \$search,
+	"check-in" => \$check_in,
+	"check-out" => \$check_out,
+	"list" => \$list,
+	"file=s" => \@file,
+	"tags=s" => \$tags,
+	'<>' => sub { $special = shift },
+) or die "Invalid parameter";
 
-print "Create new doc... ";
-my $doc = Document->new(filename => 'name',
-						filepath => '/home/wolf/dms',
-						pathname => '/home/wolf/dms/name'
-						);
-say "OK";
-
-print "Write doc... ";
-$sql->write_doc($doc);
-say "OK";
+if ($special) {
+	# one parameter without options was set.
+	# Assume it is a file path
+	# Can be used with check_out, check_in or if alone as new_document
+	if ($check_in) {
+		#code
+	}
+	elsif($check_out) {
+		
+	}
+	elsif($search) {
+		# search for a certain file name, stored in $special
+	}
+	else {
+		# ok nothing set, assume it is a path and store file in DB
+		# But first, tags can also be set on the command line to store new doc with them.
+		my $doc = Document->new(file => $special);
+		if ($tags) {
+			# check whether more than one tag was set.
+			my @tags = split(',', $tags);
+			if (1 < $#tags) {
+				# there are more than one tags set.
+				for my $tag (@tags) {
+					$doc->add_tag($tag);
+				}
+			}
+			else {
+				$doc->add_tag($tags);
+			}
+		}
+		
+		say "done";
+	}
+	
+}
